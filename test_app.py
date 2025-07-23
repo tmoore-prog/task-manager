e13ae645dbb21b1050084b8c34ab0e741578e556
Trent M <trent.moore.prog@gmail.com>
2025-07-23 16:17:23 -0600

    Add a few more tests to test suite

--- test_app.py
+++ test_app.py
@@ -1,8 +1,7 @@
 import pytest
 import json
 from config import create_app, db
-from task_models import Task
-
+from task_models import task_schema
 
 
 @pytest.fixture()
@@ -32,4 +31,56 @@ def test_create_task_success(client):
     response = client.post('/api/tasks', data=json.dumps(data),
                            content_type='application/json')
     assert response.status_code == 201
+    data = response.get_json()
+    assert 'id' in data
+    assert data['name'] == "Test POST"
 
+
+def test_get_task_by_id_success(client):
+    data = {
+        "name": "Example task",
+        "due_date": "2025-07-29"
+    }
+    task = task_schema.load(data)
+    db.session.add(task)
+    db.session.commit()
+    response = client.get('/api/tasks/1')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data['name'] == "Example task"
+    assert data['id'] == 1
+
+
+def test_update_task_success(client):
+    data = {
+        "name": "Example task",
+        "due_date": "2025-07-29"
+    }
+    task = task_schema.load(data)
+    db.session.add(task)
+    db.session.commit()
+    new_data = {
+        "name": "Example task",
+        "priority": "High"
+    }
+    response = client.put('/api/tasks/1', data=json.dumps(new_data),
+                          content_type='application/json')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data['priority'] == "High"
+    assert data['name'] == "Example task"
+    assert data['id'] == 1
+
+
+def test_task_delete_success(client):
+    data = {
+        "name": "Example task",
+        "due_date": "2025-07-29"
+    }
+    task = task_schema.load(data)
+    db.session.add(task)
+    db.session.commit()
+    response = client.delete('/api/tasks/1')
+    assert response.status_code == 200
+    post_delete_response = client.get('/api/tasks/1')
+    assert post_delete_response.status_code == 404
